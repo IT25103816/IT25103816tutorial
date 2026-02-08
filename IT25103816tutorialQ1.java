8b7f7e3cfea75ef616e4df4f555f3a91da909a27
IT25103816 <IT25103816@my.sliit.lk>
2026-02-08 22:03:40 +0530

    Add IT25103816tutorialQ1

---
+++ IT25103816tutorialQ1.java
@@ -0,0 +1,10 @@
+public class IT25103816tutorialQ1 {
+    public static void main(String[] args) {
+        
+    
+        int miles = 26;
+        int yards = 385;
+        double kilometers = miles * 1.609 + (yards / 1760.0) * 1.609;
+        System.out.println("Kilometers: " + kilometers);
+    }
+}
